513f7f3f8acd94e199cb9b06f4a81d5e14c7dc33
NGUYEN NHAT TAI <tainn.dv@gmail.com>
2025-05-06 13:47:43 +0700

    fix: resolve issue with version' dependencies

--- accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDto.java
@@ -2,15 +2,11 @@ package com.eazybytes.accounts.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
-import static lombok.AccessLevel.PRIVATE;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-@FieldDefaults(level = PRIVATE)
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class ResponseDto {
     String statusCode;
     String statusMsg;
